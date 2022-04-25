d9d65ad653660ea3b0ec41a49f456e8741e4ec12
carlosaw <carlosfreevg@gmail.com>
2022-04-25 09:44:43 -0400

    First commit

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://raw.githubusercontent.com/carlosaw/portfolio/master/src/img/aw.png",
+    link: "https://awregulagens.com.br",
+  },
+  {
+    id: 2,
+    img: "https://raw.githubusercontent.com/carlosaw/portfolio/master/src/img/nova-loja.png",
+    link: "http://awregulagens.com.br/nova_loja",
+  },
+  {
+    id: 3,
+    img: "https://raw.githubusercontent.com/carlosaw/portfolio/master/src/img/contaazul.png",
+    link: "http://awregulagens.com.br/contaazul",
+  },
+  {
+    id: 4,
+    img: "https://raw.githubusercontent.com/carlosaw/portfolio/master/src/img/pizza.png",
+    link: "https://carlosaw.github.io/pizza/",
+  },
+  {
+    id: 5,
+    img: "https://raw.githubusercontent.com/carlosaw/portfolio/master/src/img/canil.png",
+    link: "https://app-canil.herokuapp.com/",
+  },
+  {
+    id: 6,
+    img: "https://raw.githubusercontent.com/carlosaw/netflixclone/master/netflixclone.png",
+    link: "http://awregulagens.com.br/netflixclone/",
+  },
+];
